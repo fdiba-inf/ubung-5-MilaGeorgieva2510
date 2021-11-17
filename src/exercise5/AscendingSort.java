cb95b22a73864f8ebc12bfcb20ea62c38e32dbbf
MilaGeorgieva25 <>
2021-11-17 09:39:44 +0000

    llllll

--- src/exercise5/AscendingSort.java
+++ src/exercise5/AscendingSort.java
@@ -6,19 +6,24 @@ import java.util.Scanner;
 public class AscendingSort {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
         System.out.print("Enter number count: ");
         int numberCount = input.nextInt();
-
         int[] numbers = new int[numberCount];
-
         System.out.println("Enter numbers: ");
         for (int index = 0; index < numbers.length; index++) {
             numbers[index] = input.nextInt();
         }
-
-        // Sort numbers in ascending order
-
+        for (int index = 0; index < numbers.length - 1; index++) {
+            int minIndex = index;
+            for (int i = index + 1; i < numbers.length; i++) {
+                if (numbers[minIndex] > numbers[i]){
+                    minIndex = i;
+                }
+            }
+            int temp = numbers[index];
+            numbers[index] = numbers[minIndex];
+            numbers[minIndex] = temp;
+        }
         String numbersAsString = Arrays.toString(numbers);
         System.out.println("Sorted numbers: " + numbersAsString);
     }
--- src/exercise5/UniqueNumbers.java
+++ src/exercise5/UniqueNumbers.java
@@ -9,17 +9,26 @@ public class UniqueNumbers {
 
         System.out.print("Enter number count: ");
         int numberCount = input.nextInt();
-
         int[] numbers = new int[numberCount];
-
         System.out.println("Enter numbers: ");
         for (int index = 0; index < numbers.length; index++) {
             numbers[index] = input.nextInt();
         }
-
         int[] uniqueNumbers = new int[numbers.length];
-        // Find unique numbers in numbers
-
+        int nextUniqueIndex = 0; 
+        for ( int index = 0; index < numbers.length; index++) {
+          boolean found = false; 
+          for (int i = 0; i < uniqueNumbers.length; i++){
+            if (numbers[index]== uniqueNumbers [i]){
+              found = true;
+              break;
+            }
+          }
+           if (!found){
+             uniqueNumbers[nextUniqueIndex]= numbers[index];
+             nextUniqueIndex++;
+          }
+        }
         String uniqueNumbersAsString = Arrays.toString(uniqueNumbers);
         System.out.println("Unique numbers: " + uniqueNumbersAsString);
     }
